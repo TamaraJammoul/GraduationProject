530e4dd2458458f41d9ed7c82006611b55eb6c5f
TamaraJamoul <tamara.jamoul@wizme.com>
2021-07-30 05:46:53 +0300

    first commit

---
+++ .history/client-app/src/components/add-project-member-dialog/add-project-member-dialog.component_20210730000832.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { FormControl } from '@angular/forms';
+import { AppActions } from '../../store/app.action';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { User } from '../../models/user.model';
+import { getProjectUsersList } from '../../store/app.selector';
+
+@Component({
+  selector: 'app-add-project-member-dialog',
+  templateUrl: './add-project-member-dialog.component.html',
+  styleUrls: ['./add-project-member-dialog.component.scss']
+})
+export class AddProjectMemberDialogComponent implements OnInit {
+  control = new FormControl();
+  users: User[] = [];
+  constructor(private store$: Store,
+    public dialogRef: MatDialogRef<AddProjectMemberDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: string) { }
+
+  ngOnInit(): void {
+    this.store$.dispatch(AppActions.fetchProjectUsers(this.data));
+    this.store$.select(getProjectUsersList).subscribe(data => this.users = data);
+  }
+
+  onSubmit() {
+    this.store$.dispatch(AppActions.addProjectTeamMember(this.control.value, this.data));
+  }
+}
---
+++ .history/client-app/src/components/nodejs/nodejs.component_20210730045636.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nodejs',
+  templateUrl: './nodejs.component.html',
+  styleUrls: ['./nodejs.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class NodejsComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
